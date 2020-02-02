05eddddc808f10fa34b9c20fa0ea7d916ea4c414
Kiang Teng Lim <kiangtengl@fb.com>
2020-02-01 16:27:55 -0800

    Added ES2016 features to babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,8 @@ module.exports = {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
+          loader: "babel-loader",
+          options: require("./babel.config.json")
         }
       }
     ]
